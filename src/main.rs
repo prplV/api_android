177788e29a9a274358bec033ce6c1c9a8d73a142
prplV <maseeeeeeeed@gmail.com>
2024-10-16 13:37:28 +0300

    new deps + basic skeleton (not full) of api server

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,20 @@ mod structs;
 use structs::RequestType;
 use actix_web::{get, http::StatusCode, post, web, App, HttpResponse, HttpServer, Responder};
 use serde::{Serialize, Deserialize};
+use dotenv::dotenv;
+use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
 
 #[get("/")]
 async fn index() -> impl Responder {
     HttpResponse::build(StatusCode::OK)
         .content_type("text/html; charset=utf-8")
         .body("HELLO API")
+}
+#[get("/ping")]
+async fn ping() -> impl Responder {
+    HttpResponse::build(StatusCode::OK)
+        .content_type("text/html; charset=utf-8")
+        .body("PONG")
 }
 
 async fn check_crdts(req_body: web::Json<RequestType>) -> impl Responder {
@@ -17,15 +25,17 @@ async fn check_crdts(req_body: web::Json<RequestType>) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("Starting server on 127.0.0.1:8081");
     HttpServer::new(|| {
         App::new()
             .service(index)
-            // .service(
-            //     web::scope("/api/v1")
-            //         .route("/check_credentials", web::post().to(check_crdts))
-            // )
+            .service(ping)
+            .service(
+                web::scope("/api/v1")
+                    .route("/check_credentials", web::post().to(check_crdts))
+            )
     })
-        .bind(("127.0.0.1", 11223))?
+        .bind(("127.0.0.1", 8081))?
         .run()
         .await
 }
